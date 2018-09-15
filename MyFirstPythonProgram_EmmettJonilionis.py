f0773a1e001ff26e47fb244b811a5f20aeb2135b
Jonilioe7815 <43292890+Jonilioe7815@users.noreply.github.com>
2018-09-15 12:14:00 +0200

    Add files via upload

---
+++ MyFirstPythonProgram_EmmettJonilionis.py
@@ -0,0 +1,6 @@
+#This is a program that is simple and displays a string of text to read
+#08SEP2018
+#CTI-110 Tutorial 1 - My First Python Program
+#Emmett Jonilionis
+#
+print('This is a test of IDLE.')
